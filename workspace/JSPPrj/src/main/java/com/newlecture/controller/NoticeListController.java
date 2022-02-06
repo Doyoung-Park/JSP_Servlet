37ff353eaec0880e5ed7f059ec9a9f350dd05ff8
Doyoung Park <doodoo55@gachon.ac.kr>
2022-02-06 14:21:52 +0900

    Added searching function in the notice-list page

--- workspace/JSPPrj/src/main/java/com/newlecture/controller/NoticeListController.java
+++ workspace/JSPPrj/src/main/java/com/newlecture/controller/NoticeListController.java
@@ -24,9 +24,22 @@ public class NoticeListController extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		NoticeService service= new NoticeService();
+		// list?f=title&q=a 의 형태로 브라우저에서 controller 로 전달됨.
+		String field_ = request.getParameter("f");
+		String query_ = request.getParameter("q");
 		
-		List<Notice> list= service.getNoticeList();
+		// request의 parameter로 받은 값이 null일 수도 있음 ->
+		// null 일 경우를 대비해 초기화값을 정해줌
+		String field="title";
+		if(field_ !=null)
+			field=field_;
+		
+		String query ="";
+		if(query_!=null)
+			query=query_;
+		
+		NoticeService service= new NoticeService();
+		List<Notice> list= service.getNoticeList(field, query,1);
 		
 		
 		
