0df8b420587103af0d9bf89b43d3bac7193bd577
Doyoung Park <doodoo55@gachon.ac.kr>
2022-02-18 00:32:35 +0900

    added dealing with the last number of Pager

--- workspace/JSPPrj/src/main/java/com/newlecture/web/service/NoticeService.java
+++ workspace/JSPPrj/src/main/java/com/newlecture/web/service/NoticeService.java
@@ -111,7 +111,8 @@ public class NoticeService {
 			
 			ResultSet rs = st.executeQuery();
 
-			count = rs.getInt("count");
+			if(rs.next())
+				count = rs.getInt("count");
 
 			rs.close();
 			st.close();
