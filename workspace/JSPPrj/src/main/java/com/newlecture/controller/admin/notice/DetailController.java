ab3a6233d6964245a06cf285f6a4ae1efe679c17
Doyoung Park <doodoo55@gachon.ac.kr>
2022-02-21 00:58:50 +0900

    관리자- 공지사항 리스트에서 공지사항을 클릭했을 때 그 공지사항에 대한 자세한 페이지(detail)페이지 추가함 (detail.jsp)

---
+++ workspace/JSPPrj/src/main/java/com/newlecture/controller/admin/notice/DetailController.java
@@ -0,0 +1,44 @@
+package com.newlecture.controller.admin.notice;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
+
+@WebServlet("/admin/board/notice/detail")
+public class DetailController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int id=Integer.parseInt(request.getParameter("id"));
+		
+		NoticeService service = new NoticeService();
+		Notice notice = service.getNotice(id);
+		request.setAttribute("n", notice);
+		
+		// 서블릿에서 서블릿으로 전이하는 방법은 2가지가 있음
+		// 1. redirect 2. forward
+		
+		// redirect: 이 페이지에서 저 페이지로 보내버리는 방법
+		// ex. 공지사항 페이지에서 로그인페이지로 넘기는 작업
+		
+		// forward: 이 페이지에서 작업하던 내용을 저 페이지에서 이어받아서 작업하는 방식
+		
+		
+		// forward
+		request
+		.getRequestDispatcher("/WEB-INF/view/admin/board/notice/detail.jsp")
+		.forward(request, response);
+	}
+}
