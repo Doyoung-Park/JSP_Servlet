0ee5c9fceb137b09b3a1ef7d0e45ae64ada099cb
Doyoung Park <doodoo55@gachon.ac.kr>
2022-02-21 03:38:51 +0900

    관리자 - 글쓰기 - 업로드한 파일 저장 위치 설정 바이트 스트림 & 버퍼 이용 -> 업로드한 파일, 데이터베이스 저장됨.

--- workspace/JSPPrj/src/main/java/com/newlecture/controller/admin/notice/RegController.java
+++ workspace/JSPPrj/src/main/java/com/newlecture/controller/admin/notice/RegController.java
@@ -1,6 +1,9 @@
 package com.newlecture.controller.admin.notice;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -9,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
@@ -36,10 +40,33 @@ public class RegController extends HttpServlet{
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String title = request.getParameter("title");
 		
-		System.out.println("title : " + title);
+	//	System.out.println("title : " + title);
 		
 		String content = request.getParameter("content");
 		String isOpen = request.getParameter("open");	// true 또는 null 값이 옴
+		
+		Part filePart = request.getPart("file");
+		String fileName = filePart.getSubmittedFileName();
+		InputStream fis = filePart.getInputStream();	// 바이너리 스트림 설정
+		
+		// 파일이 저장되는 경로는 상대 경로가 될 수 없음. 절대 경로로 설정해야 함
+		String realPath = request.getServletContext().getRealPath("/upload");	// 저장 위치의 절대 경로를 이 코드를 통해 쉽게 알아낼 수 있음
+	//	System.out.println(realPath);	
+		
+	//	fis.read();		// 1바이트씩 읽어들임. 다 읽어들인 후에는 -1을 리턴
+		
+		String filePath = realPath + File.separator + fileName;
+		FileOutputStream fos = new FileOutputStream(filePath);
+		
+		
+		byte[] buf = new byte[1024];
+		int size = 0;
+		while((size=fis.read(buf))!= -1) 
+			fos.write(buf, 0,size);
+		
+		fos.close();
+		fis.close();
+		
 		boolean pub = false;
 		
 		if(isOpen!=null)
